dacb2edc8ea43b9c2ce82535dc5baa0e7449536b
Christopher Morejon <Christophermorejon17@gmail.com>
2022-12-15 02:05:31 +0000

    2nd Commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Vehicles
 #from models import Person
 
 app = Flask(__name__)
@@ -16,7 +16,8 @@ app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -26,16 +27,58 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+# Characters HERE
+characters = [
+    {
+        "id": 1,
+        "name": 'Luke Skywalker'
+    },
+    {
+        "id": 2,
+        "name": 'Darth Vader'
+    }
+]
+
+# Planets HERE
+planets = [
+    {
+        "id": 1,
+        "name": 'Tatooine'
+    },
+    {
+        "id": 2,
+        "name": 'Mustafar'
+    }
+]
+
+# Vehicles HERE
+vehicles = [
+    {
+        "id": 1,
+        "name": 'X-34 Landspeeder'
+    },
+    {
+        "id": 2,
+        "name": 'Death Star'
+    }
+]
+
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
+# Users HERE
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -44,6 +87,25 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Characters HERE
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    json_text = jsonify(characters)
+    return json_text, 200
+
+# Planets HERE
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    json_text = jsonify(planets)
+    return json_text, 200
+
+# Vehicles HERE
+@app.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    json_text = jsonify(vehicles)
+    return json_text, 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,7 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Users HERE
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -16,4 +18,49 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+# Characters HERE
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+# Planets HERE
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+# Vehicles HERE
+class Vehicles(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
         }
